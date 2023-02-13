a30036fbca6bd3edfbb4d29dbe08cd0f998424d9
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-02-13 16:22:40 +0100

    Fix ESlint errors

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // Check if book collection is already stored in local storage
-let bookCollection = JSON.parse(localStorage.getItem("bookCollection")) || [];
+let bookCollection = JSON.parse(localStorage.getItem('bookCollection')) || [];
 
 // Display all books in the collection
 const displayBooks = () => {
-  const bookList = document.getElementById("book-list");
-  bookList.innerHTML = "";
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
   bookCollection.forEach((book) => {
-    const bookElement = document.createElement("div");
-    bookElement.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><button onclick="removeBook('${book.title}')">Remove</button><hr>`;
+    const bookElement = document.createElement('div');
+    bookElement.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><button onclick="removeBook("${book.title}")">Remove</button><hr>`;
     bookList.appendChild(bookElement);
   });
 };
 
 // Add book to collection
 const addBook = () => {
-  const title = document.getElementById("title-input").value;
-  const author = document.getElementById("author-input").value;
+  const title = document.getElementById('title-input').value;
+  const author = document.getElementById('author-input').value;
   // if book already exists in collection, don't add it again
   if (bookCollection.some((book) => book.title === title)) return;
 
   bookCollection.push({ title, author });
-  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
   displayBooks();
 };
 
 // Remove book from collection
 const removeBook = (title) => {
   bookCollection = bookCollection.filter((book) => book.title !== title);
-  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
   displayBooks();
 };
 
 // Add event listeners to buttons
-document.getElementById("add-button").addEventListener("click", addBook);
+document.getElementById('add-button').addEventListener('click', addBook);
 
 // Display books on page load
 displayBooks();
