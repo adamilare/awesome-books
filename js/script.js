d2c3204b46dcc0f0430b0ae9b0049ba8ea86d020
adarachel <adarachelofficial@gmail.com>
2023-02-14 15:21:10 +0100

    fixed linter errors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-import Book from "./Book.js";
-import BookCollection from "./BookCollection.js";
+import Book from './Book.js';
+import BookCollection from './BookCollection.js';
 
 const bookCollection = new BookCollection();
 
 // Add book to collection
 const addBook = () => {
-  const title = document.getElementById("title-input").value;
-  const author = document.getElementById("author-input").value;
+  const title = document.getElementById('title-input').value;
+  const author = document.getElementById('author-input').value;
 
   bookCollection.addBook(new Book(title, author));
 
@@ -22,13 +22,13 @@ const removeBook = (title) => {
 
 // Display all books in the collection
 const displayBooks = () => {
-  const bookList = document.getElementById("book-list");
-  bookList.innerHTML = "";
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
   bookCollection.books.forEach((book) => {
-    const bookElement = document.createElement("div");
-    bookElement.classList.add("book-item");
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book-item');
     bookElement.innerHTML = `<p><span class="book-title">"${book.title}" </span> <span class="book-author"> by ${book.author} </span><button data-ref="${book.title}">Remove</button> </p>`;
-    bookElement.querySelector("button").addEventListener("click", () => {
+    bookElement.querySelector("button").addEventListener('click', () => {
       removeBook(book.title);
     });
     bookList.appendChild(bookElement);
@@ -36,7 +36,7 @@ const displayBooks = () => {
 };
 
 // Add event listeners to buttons
-document.getElementById("add-button").addEventListener("click", addBook);
+document.getElementById('add-button').addEventListener('click', addBook);
 
 // Display books on page load
 displayBooks();
